be9c1ff8270b0e03f610423ae99c0640af0bb05d
Oscar Bartra <obartra@gmail.com>
2017-02-18 17:54:54 -0800

    wip

---
+++ src/image/webp.js
@@ -0,0 +1,7 @@
+/**
+ * This will probably require a separate build for Node / web (much like readpixels)
+ * 
+ * https://github.com/FallingSnow/webp-polyfill/blob/master/webp-polyfill.js is using the library in a similar way
+ * 
+ * Added sample data to spec/samples/webp from https://developers.google.com/speed/webp/gallery2
+ */
